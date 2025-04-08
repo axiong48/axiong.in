23a87d6a4ad0223e41ee2085dbd7d450f3b57774
axiong48 <aaronxiong1013@gmail.com>
2025-04-08 12:01:34 -0700

    Update Home.tsx

--- axiongweb/src/components/Home.tsx
+++ axiongweb/src/components/Home.tsx
@@ -26,7 +26,7 @@ const Home = () => {
           </a>
         </div>
 
-        <img src="/pfp.png" alt="Aaron Xiong Profile Pic" className="profile-pic" />
+        <img src="/Aesirk.png" alt="Aaron Xiong Profile Pic" className="profile-pic" />
 
         <h5 className="profile-heading">
           <strong>3rd Year Computer Science Engineering At UC Merced</strong>
